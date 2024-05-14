b813b575b19ffa5fa03aa1e107ee23a54c79879a
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-05-14 08:45:05 -0300

    adding boards banner for not logged user

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -8,6 +9,11 @@ module.exports = {
     "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
+    colors: {
+      ...colors,
+      "eb-pink": "#f42d7e",
+
+    },
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
